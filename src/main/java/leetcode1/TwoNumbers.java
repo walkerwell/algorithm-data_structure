9a498d1f2149556b577bcdfe2c7fb63b4d69f180
wubo <wubo@daojia.com>
2017-04-25 10:33:54 +0800

    start  code 'add two numbers'

--- src/main/java/leetcode1/TwoNumbers.java
+++ src/main/java/leetcode1/TwoNumbers.java
@@ -1,3 +1,5 @@
+package leetcode1;
+
 import org.junit.Test;
 
 import java.util.HashMap;
---
+++ src/main/java/leetcode2/AddTwoNumbers.java
@@ -0,0 +1,38 @@
+package leetcode2;
+
+import org.junit.Test;
+
+/**
+ * Created by wubo on 2017-4-25.
+ */
+public class AddTwoNumbers {
+    /**
+     *
+     You are given two non-empty linked lists representing two non-negative integers.
+     The digits are stored in reverse order and each of their nodes contain a single digit.
+     Add the two numbers and return it as a linked list.
+     You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+     Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+     Output: 7 -> 0 -> 8
+
+     */
+    @Test
+    public void testRun(){
+
+    }
+
+    /**
+     Definition for singly-linked list.
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        ListNode listNode = new ListNode(1);
+
+        return listNode;
+    }
+}
