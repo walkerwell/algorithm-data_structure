70a01f986f4135f5b6d635904c66a16af815a3ac
wubo <wubo@daojia.com>
2017-06-16 16:15:15 +0800

    add problem fix thought

--- src/main/python/codeforces/run_814A.py
+++ src/main/python/codeforces/run_814A.py
@@ -15,3 +15,7 @@ for index in xrange(0,len(a)-1):
         print("YES")
         exit(0)
 print("NO")
+# 此题的题目含义是将b数组插入到A数组，直到A数组是非递增的就可以
+# 将B排序后，reverse
+# 再往A中插入，每次插入判断前后两个数组是否是递减的 递减就返回YES 程序结束
+# 当循环A数据结束 说明没有找到非递增数组，则NO
