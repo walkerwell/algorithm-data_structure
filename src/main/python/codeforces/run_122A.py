bc021c749f70a3f3ef0807886bb1416b7549fb99
walker <769898554@qq.com>
2018-06-06 01:02:03 +0800

    update

--- src/main/python/codeforces/run_122A.py
+++ src/main/python/codeforces/run_122A.py
@@ -1,12 +1,19 @@
+# n=input()
+# def isLucky(n):
+#     s=""+(str)(n)
+#     for i in s:
+#         if(i<>'4' and i<>'7'):
+#             return False
+#     return True
+# for i in range(n+1):
+#     if(isLucky(i) and n%i==0):
+#         print "YES"
+#         exit(0)
+# print "NO"
+lucky=[4,7,47,74,447,474,477,744,747,774]
 n=input()
-def isLucky(n):
-    s=""+(str)(n)
-    for i in s:
-        if(i<>'4' and i<>'7'):
-            return False
-    return True
-for i in range(n+1):
-    if(isLucky(i) and n%i==0):
+for i in lucky:
+    if n%i==0:
         print "YES"
         exit(0)
 print "NO"
