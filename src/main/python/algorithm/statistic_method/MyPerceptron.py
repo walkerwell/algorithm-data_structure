22935c7ba75020fb8047ee1ce6fa3223bec512c8
Denny <wubo@taoche.com>
2019-05-10 19:57:14 +0800

    perceptron final commit

--- src/main/python/algorithm/statistic_method/MyPerceptron.py
+++ src/main/python/algorithm/statistic_method/MyPerceptron.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 import numpy as np
 import pandas as pd
-from sklearn.metrics import precision_score,recall_score,accuracy_score,confusion_matrix
+import time
+from sklearn.metrics import accuracy_score,recall_score,precision_score,confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Perceptron as sk_perceptron
@@ -19,6 +20,7 @@ class MyPerceptron():
         return np.where(sign_res>0,1,-1)
 
     def fit(self,X,y):
+        print("perceptron is running")
         self.w = np.zeros(len(X[0]),dtype=float)
         iter = 0
         while( iter<= self.max_iter_num):
@@ -30,6 +32,7 @@ class MyPerceptron():
                     wrongNum += 1
                     self.w = self.w + self.learning_rate * np.dot(label,x)
                     self.b = self.b + self.learning_rate * label
+            # print("now is running iter in:",iter)
             iter += 1
             if wrongNum == 0:
                 break
@@ -40,13 +43,14 @@ class MyPerceptron():
         return self.sign(np.dot(X,self.w)+self.b)
 
 def read_data():
-    df = pd.read_csv("preceptron_train.csv")
-    X = df.drop(['label'], axis=1).values
-    y = df['label']
-    y = np.array(y)*2-1
-    ss = StandardScaler()
-    X = ss.fit_transform(X)
-    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.5,random_state=2019)
+    data_clos = ['label','sell_price','age','sell_speed','mileage','avg_mileage']
+    train_data = pd.read_excel('../data/Perceptron_data.xlsx', header=None, sheet_name='TRAIN_DATA',names=data_clos)
+    test_data = pd.read_excel('../data/Perceptron_data.xlsx', header=None, sheet_name='TEST_DATA',names=data_clos)
+    stdscaler = StandardScaler()
+    train_x = stdscaler.fit_transform(train_data.drop(['label'],axis=1))
+    train_y = train_data['label']*2-1
+    test_x = stdscaler.transform(test_data.drop(['label'],axis=1))
+    test_y = test_data['label']*2-1
     return train_x, test_x, train_y, test_y
 
 if __name__ == '__main__':
@@ -62,10 +66,13 @@ if __name__ == '__main__':
     print("MyPerceptron test precision:", precision_score(test_y, test_pred))
     print("MyPerceptron test recall:", recall_score(test_y, test_pred))
     print("MyPerceptron test aucc:", accuracy_score(test_y, test_pred))
+    print("MyPerceptron train aucc:",accuracy_score(train_y,train_pred))
 
     # use sk_learn Perceptron compute aucc
     clf = sk_perceptron()
+    start = time.time()
     clf.fit(train_x,train_y)
+    print("fit cost time:",(time.time() - start)/60)
     train_pred = clf.predict(train_x)
     test_pred = clf.predict(test_x)
     print("sk_perceptron train accu:",accuracy_score(train_y,train_pred))
