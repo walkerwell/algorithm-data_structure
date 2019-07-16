3edaff8ce6416e8d221fde2ee4175d1e3333d4d0
Denny <769898554@qq.com>
2019-07-16 13:37:27 +0800

    update

--- src/main/python/codeforces/run_799A.py
+++ src/main/python/codeforces/run_799A.py
@@ -1,21 +1,21 @@
 # -*- coding:utf-8 -*-
 # 将每行最小和每列最小再输入的时候选出来
-matrix= map(int,raw_input().split())
+matrix= map(int,input().split())
 sum = matrix[0]
 each_time=matrix[1]
 each_time_bake_num=matrix[2]
 second_oven_build_time=matrix[3]
 if sum <  each_time_bake_num:
-    print "NO"
+    print ("NO")
     exit(0)
 one_oven_bake_num = (sum+each_time_bake_num-1)/each_time_bake_num
 if one_oven_bake_num < 2:
-    print "NO"
+    print ("NO")
     exit(0)
 one_oven_use_time=one_oven_bake_num * each_time
 emp=0
 emp = each_time - second_oven_build_time
 if one_oven_use_time <= (each_time + second_oven_build_time - emp):
-    print "NO"
+    print ("NO")
     exit(0)
-print "YES"
+print ("YES")
